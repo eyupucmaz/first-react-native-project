a93ccdb6dba88e16ed12f364635cc1aba6d7137e
eyupucmaz <eyupucmaz@gmail.com>
2020-07-25 23:31:38 +0300

    Added new components.

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.top}>
+				<Text
+					style={{
+						fontSize: 30,
+					}}
+				>
+					Your Name
+				</Text>
+				<TextInput style={styles.textInput} />
+			</View>
+			<View style={styles.middle}>
+				<Text
+					style={{
+						fontSize: 30,
+					}}
+				>
+					Your Mail
+				</Text>
+				<TextInput keyboardType="email-address" style={styles.textInput} />
+			</View>
+			<View style={styles.bottom}>
+				<Text
+					style={{
+						fontSize: 30,
+					}}
+				>
+					Your Password
+				</Text>
+				<TextInput keyboardType="number-pad" style={styles.textInput} />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		backgroundColor: "#e0e0e0",
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "space-between",
+	},
+	top: {
+		margin: 20,
+		flex: 0.3,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#5dd39e",
+		borderWidth: 5,
+		borderColor: "#000",
+		borderTopLeftRadius: 40,
+		borderTopRightRadius: 40,
+	},
+	middle: {
+		margin: 20,
+		flex: 0.3,
+		backgroundColor: "#525174",
+		borderWidth: 5,
+		borderColor: "#000",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	bottom: {
+		margin: 20,
+		flex: 0.3,
+		backgroundColor: "#348aa7",
+		borderWidth: 5,
+		borderColor: "#000",
+		borderBottomLeftRadius: 30,
+		borderBottomRightRadius: 30,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	textInput: {
+		height: 30,
+		width: 200,
+		fontSize: 20,
+		borderWidth: 1,
+		borderColor: "#000",
+		backgroundColor: "#e0e0e0",
+	},
 });
